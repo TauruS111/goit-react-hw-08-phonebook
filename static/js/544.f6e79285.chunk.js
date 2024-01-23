c022728b95cbf01c57b2f6c2c942a906b993c730
TauruS111 <134150044+TauruS111@users.noreply.github.com>
2024-01-23 22:07:23 +0000

    Deploying to gh-pages from @ TauruS111/goit-react-hw-08-phonebook@4d3669a906cc97d31a049e232ed50925c7318a6e 🚀

---
+++ static/js/544.f6e79285.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{9544:function(e,t,n){n.r(t);n(2791);var o=n(184);t.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{style:{color:"green"},children:"Welcome to the Phonebook !"}),(0,o.jsx)("h2",{children:"\u260e\ufe0f Enjoy Using \ud83d\udcd7"})]})}}}]);
+//# sourceMappingURL=544.f6e79285.chunk.js.map
